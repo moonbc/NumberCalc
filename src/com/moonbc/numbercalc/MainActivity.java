907c0a2f9d83913668eff4fe8fd4ba87c5df652d
moonbc <lightbc@gmail.com>
2013-08-09 14:38:20 +0900

    Call game activity

--- src/com/moonbc/numbercalc/MainActivity.java
+++ src/com/moonbc/numbercalc/MainActivity.java
@@ -2,6 +2,7 @@ package com.moonbc.numbercalc;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -24,7 +25,11 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
+				//Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
+				Intent intent = new Intent(MainActivity.this, GameActivity.class);
+				startActivity(intent);			
+				
+				
 			}
 		} );
 		
