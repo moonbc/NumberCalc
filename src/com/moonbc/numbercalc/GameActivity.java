0754f4c12f70d5bf8272507a21ed9cfdfa1bc50a
moonbc <lightbc@gmail.com>
2013-08-09 13:21:01 +0900

    Add GameActivity file

---
+++ src/com/moonbc/numbercalc/GameActivity.java
@@ -0,0 +1,18 @@
+package com.moonbc.numbercalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class GameActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_game);
+	
+	}
+	
+
+}
