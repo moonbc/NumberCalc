83d36e7a19fb329bce4ec91c462d56ce83486cb5
moonbc_home <lightbc@gmail.com>
2013-08-12 02:13:28 +0900

    GameActivity UI update

--- src/com/moonbc/numbercalc/GameActivity.java
+++ src/com/moonbc/numbercalc/GameActivity.java
@@ -2,6 +2,7 @@ package com.moonbc.numbercalc;
 
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -13,6 +14,17 @@ public class GameActivity extends Activity {
 	int value =0;
 	TextView mTimeCount;
 	
+	Button mNumber1Button;
+	Button mNumber2Button;
+	Button mNumber3Button;
+	Button mNumber4Button;
+	
+	
+	
+	
+	
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,6 +33,22 @@ public class GameActivity extends Activity {
 		mTimeCount = (TextView)findViewById(R.id.time_counter_text);
 		mHandler.sendEmptyMessage(0);
 		
+		
+		mNumber1Button = (Button)findViewById(R.id.number1_button);
+		
+		
+		mNumber1Button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//mNumber1Button.setBackgroundColor(Color.BLUE);
+
+				mNumber1Button.setEnabled(false);
+				Toast.makeText(getApplicationContext(), "button1", Toast.LENGTH_LONG).show();
+			}
+		});		
+			
 	
 	}
 	Handler mHandler = new Handler() {
---
+++ src/com/moonbc/numbercalc/NumberSet.java
@@ -0,0 +1,5 @@
+package com.moonbc.numbercalc;
+
+public class NumberSet {
+
+}
